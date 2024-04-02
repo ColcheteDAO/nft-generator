66745eb2062ff9e6cdaacfebc0a41fdb3c39d4d7
juancolchete <juanudkp@gmail.com>
2024-04-02 21:31:32 +0000

    feat: bash sample

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello world")
+#! /usr/bin/env node
+const cliFunctions:any = {
+  helloWorld: ()=>{
+    console.log("Hello world")
+  } 
+}
+
+const cliArgs = process.argv.slice(2)
+cliFunctions[cliArgs[0]]();
