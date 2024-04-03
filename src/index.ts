c796f966f5f5e643537c002c8b3dfac141c1c659
juancolchete <juanudkp@gmail.com>
2024-04-03 01:02:34 +0000

    feat: generate based on size files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 #! /usr/bin/env node
 import File from "./models/File"
 import nftgen from "./nftgen.json"
+const templates = { 
+  single: (nftgenItem:any) =>{
+    for(let i=0; i < nftgenItem.size; i++){
+      File.generateFile(`out/${nftgenItem.folder}/${i}`,JSON.stringify(nftgenItem,null,2)); 
+    }
+  }
+}
+type templateOptions = keyof typeof templates;
 const cliFunctions:any = {
   metadata: ()=>{
     for(let i=0;i < nftgen.length; i++){
       File.createDir(`out/${nftgen[i].folder}`)
+      const template: templateOptions = nftgen[i].template as templateOptions;
+      if(Object.keys(templates).indexOf("single") > -1){
+        templates[template](nftgen[i])
+      }
     }
   } 
 }
