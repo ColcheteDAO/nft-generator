862888b81890a01ef9389b7a7aa9907712804c95
juancolchete <juanudkp@gmail.com>
2024-04-03 21:13:51 +0000

    feat: IMX single model

--- README.md
+++ README.md
@@ -22,15 +22,15 @@ To configure metadata create an object inside `nftgen.json` based on template, t
 
 ### singleIMX
 **template**: `string` represented as "singleIMX" 
+**folder**: `string` folder name to generate metadata inside out/{folder},
 **owner_address**: `string` ether address of NFT receiver
 **size**: `number` quantity of NFTs metadata to generate,
-**metadata**: `object` with repeating NFT properties
-            **image**: `string` image on IPFS of NFT repeat for all,
-            **animation_url**: `string` animation of NFT repeat for all,
-            **youtube_url**: `string` youtube video of NFT repeat for all,
-            **name**: `string` name of NFT repeat for all but append with index "{name} #{index}",
-            **description**: `string` description of NFT item repeat for all,
-            **external_url**: `string` collection site,           
-            **attributes**: `array` with objects with 
-                **trait**: `string` attribute of NFT 
-                **value**: `string` value of NFT attribute
+**image**: `string` image on IPFS of NFT repeat for all,
+**animation_url**: `string` animation of NFT repeat for all,
+**youtube_url**: `string` youtube video of NFT repeat for all,
+**name**: `string` name of NFT repeat for all but append with index "{name} #{index}",
+**description**: `string` description of NFT item repeat for all,
+**external_url**: `string` collection site,           
+**attributes**: `array` with objects with 
+    **trait**: `string` attribute of NFT 
+    **value**: `string` value of NFT attribute
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 import File from "./models/File"
 import nftgen from "./nftgen.json"
+import { v4 } from "uuid"
 const templates = { 
   single: (nftgenItem:any) =>{
     for(let i=0; i < nftgenItem.size; i++){
@@ -13,6 +14,28 @@ const templates = {
       }
       File.generateFile(`out/${nftgenItem.folder}/${i}`,JSON.stringify(nftMetadata,null,2)); 
     }
+  },
+  singleIMX: (nftgenItem:any) =>{
+    const fullMetadata = []
+    for(let i=0; i < nftgenItem.size; i++){
+      const reference_id = v4();
+      const nftMetadata = {
+        owner_address: nftgenItem.owner_address,
+        reference_id,
+        token_id: `${i}`,
+        metadata:{
+          image: nftgenItem.image,
+          animation_url: nftgenItem.animation_url,
+          youtube_url: nftgenItem.youtube_url,
+          name: `${nftgenItem.name} #${i}`,
+          description: nftgenItem.description,
+          external_url: nftgenItem.external_url,
+          attributes: nftgenItem.attributes
+        }
+      }
+      fullMetadata.push(nftMetadata)
+    }
+    File.generateFile(`out/${nftgenItem.folder}/metadata.json`,JSON.stringify(fullMetadata,null,2)); 
   }
 }
 type templateOptions = keyof typeof templates;
@@ -21,7 +44,7 @@ const cliFunctions:any = {
     for(let i=0;i < nftgen.length; i++){
       File.createDir(`out/${nftgen[i].folder}`)
       const template: templateOptions = nftgen[i].template as templateOptions;
-      if(Object.keys(templates).indexOf("single") > -1){
+      if(Object.keys(templates).indexOf(nftgen[i].template) > -1){
         templates[template](nftgen[i])
       }
     }
